e455a0770811b2fa47660d78ffdf66a5dd7a1826
aviral1702 <aviralchandra18@gmail.com>
2024-03-04 20:04:30 +0530

    todo list

--- src/app/todolist/page.jsx
+++ src/app/todolist/page.jsx
@@ -4,28 +4,65 @@ import React, { useState } from 'react'
 const TodoList = () => {
 
     const [taskList, setTaskList] = useState([
-        {text: 'Do Coding', completed: false},
-        {text: 'Play Games', completed: false},
-        {text: 'Bring Milk', completed: false},
+        { text: 'Do Coding', completed: false },
+        { text: 'Play Games', completed: false },
+        { text: 'Bring Milk', completed: false },
     ]);
 
     const addTask = (e) => {
-        console.log(e.key);
+        if (e.code === 'Enter') {
+            console.log(e.target.value);
+            const obj = { text: e.target.value, completed: false };
+            setTaskList([obj, ...taskList]);
+            e.target.value = '';
+        }
     }
-  return (
-    <div className='bg-danger-subtle vh-100'>
-        <div className='container py-5'>
-            <h1 className='display-3 fw-bold text-center'>Todo List</h1>
-            <div className='card-shadow'>
-                <div className='card-header py-3'>
-                    <input type="text" className='form-control border-primary border-3' onKeyDown={addTask} />
-                </div>
-                <div className='card-body'></div>
-            </div>
-        </div>
 
-    </div>
-  )
+    const deleteTask = (index) => {
+        console.log(index);
+        const temp=taskList;
+        temp.splice(index,1);
+        setTaskList([...temp]);
+    }
+
+    const completeTask = (index) => {
+        const temp=taskList;
+        temp[index].completed=true;
+        setTaskList([...temp]);
+        console.log(temp);
+    }
+
+    return (
+        <div className='bg-danger-subtle vh-100'>
+            <div className='container py-5'>
+                <h1 className='display-3 fw-bold text-center'>Todo List</h1>
+                <div className='card-shadow'>
+                    <div className='card-header py-3'>
+                        <input type="text" className='form-control border-primary border-3' onKeyDown={addTask} />
+                    </div>
+                    <div className='card-body'>
+                        {
+                            taskList.map((task, index) => {
+                                return <div key={index} className='mb-4 d-flex justify-content-between align-items-center'>
+                                    <p className='my-auto'>{task.text}</p>
+                                    { task.completed ?
+                                    <span className='badge bg-success'>Completed</span>
+                                    : <span className='badge bg-warning'>Pending</span>}
+                                    <button 
+                                    className='btn btn-danger' 
+                                    onClick={() => { deleteTask(index) }}>Delete</button>
+                                    <button
+                                    onClick={()=> {completeTask(index)}}
+                                    className='btn btn-primary'>Complete Task</button>
+                                </div>;
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default TodoList
